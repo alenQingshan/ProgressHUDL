5ad2913a3bd10ccbe72c08bc1057ed4ce9dd0426
alenQingshan <liuqingshan@liuqingshandemac-mini.local>
2016-03-29 12:07:28 +0800

    Initial Commit

---
+++ ProgressHUDL/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ProgressHUDL
+//
+//  Created by 刘青山 on 16/3/29.
+//  Copyright © 2016年 刘青山. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
